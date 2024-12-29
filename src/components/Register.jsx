6f38217c0068f658a96db5bc8274618238f42fa1
EngineerMoore <alexiashalise@gmail.com>
2024-12-29 12:31:45 -0600

    added functional registration form

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,7 +1,88 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Register = () => {
+  const [firstName, setFirstName ] = useState(``);
+  const [lastName, setLastName ] = useState(``);
+  const [username, setUsername ] = useState(``);
+  const [email, setEmail ] = useState(``);
+  const [password, setPassword ] = useState(``);
+  const [registerErr, setRegisterErr] = useState(``);
+  const navigate = useNavigate();
+
+
+  const createAccount = async (e) => {
+    e.preventDefault();
+    console.log(`${firstName} ${lastName} + ${username} + ${email} + ${password}`);
+    try {
+      const response = await fetch(``, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          firstName: firstName,
+          lastName: lastName,
+          username: username,
+          email: email,
+          password: password,
+        })
+      })
+      const result = await response.json();
+      console.log(result);
+      setFirstName("");
+      setLastName("");
+      setUsername("");
+      setEmail("");
+      setPassword("");
+      switch (!result.token){
+        case true:
+          setRegisterErr(result.message);
+          break;
+        default:
+          navigate(`/profile`);
+      }
+    }catch(e){console.error(e.message)};
+  }
+
   return (
     <>
-
+      <form onSubmit={createAccount}>
+        <h2>Create an account</h2>
+        <input
+          type="text"
+          value={firstName}
+          placeholder="First Name"
+          onChange={(e) => setFirstName(e.target.value)}
+        />
+        <input
+          type="text"
+          value={lastName}
+          placeholder="Last Name"
+          onChange={(e) => setLastName(e.target.value)}
+        />
+        <input
+          type="text"
+          value={username}
+          placeholder="Username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="email"
+          value={email}
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+        // TODO: type="password"
+          type="text"
+          value={password}
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button>Sign Up</button>
+      </form>
+      <p>{registerErr}</p>
     </>
   )
 }
