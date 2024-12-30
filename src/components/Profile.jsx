2fe951279859cbaaa936d5eda2ad8e1e06185a5a
EngineerMoore <alexiashalise@gmail.com>
2024-12-29 18:43:03 -0600

    added params and play button to profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 const Profile = () => {
+  const { username } = useParams();
   return (
     <>
-
+      <h2>Welcome, {username}</h2>
+      <h3>First Name</h3>
+      <h3>Last Name</h3>
+      <button>Play Game</button>
     </>
   )
 }
