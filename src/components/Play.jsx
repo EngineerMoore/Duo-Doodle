4a91b05043c12be53a7f6f8353731419766833f7
EngineerMoore <alexiashalise@gmail.com>
2025-04-17 09:14:15 -0500

    added ternary statements for all elements/components that changed based on role

--- src/components/Play.jsx
+++ src/components/Play.jsx
@@ -1,17 +1,21 @@
 import { useState, useRef, useEffect } from "react";
+import { useNavigate } from "react-router";
 import { socket } from "../socket";
 import Artist from "./Artist";
 import Guess from "./Guess";
 import Canvas from "./Canvas";
 
-const Play = () => {
+const Play = ({correctAnswer, setCorrectAnswer}) => {
   const canvasRef = useRef(null);
+  const Ref = useRef(null);
   const [selectedTool, setSelectedTool] = useState("brush");
   const [fillColorChecked, setFillColorChecked] = useState(false);
   const [brushWidth, setBrushWidth] = useState(5);
   const [renderColor, setRenderColor] = useState(`#000`);
-  const [player, setPlayer] = useState();
-  
+  const [player, setPlayer] = useState('artist');
+  const [timer, setTimer] = useState(`2:00`);
+  const navigate = useNavigate();
+
   useEffect(() => {
     socket.on('artist', () => {
       setPlayer('artist');
@@ -26,8 +30,57 @@ const Play = () => {
       socket.off('guesser');
       }
   }, [])
+
+  useEffect(() => {
+    let timeRemaining = 360;
+
+    const timeDecrement = () => {
+      if (timeRemaining <= 0) {
+        setTimer(`0:00`);
+        clearInterval(startTimer);
+        return;
+      }
+
+      if (timeRemaining <= 30) {
+        const addAlert = () => {
+          const timerElement = document.querySelector(`.timer`);
+          timerElement.id = "timer-alert";
+          Ref.current = timerElement.id;
+        };
+        if (!Ref.current) addAlert();
+      }
+
+      timeRemaining--;
+
+      const minutes = Math.floor(timeRemaining / 60);
+      const seconds = timeRemaining % 60;
+
+      const clock = `${minutes}:${seconds > 9 ? seconds : `0${seconds}`}`;
+      setTimer(clock);
+
+    }
+
+    const startTimer = setInterval(timeDecrement, 1000);
+
+    return () => clearInterval(startTimer);
+  }, []);
+
+  if (timer === '0:00') {
+    navigate("/results")
+  };
+
   return (
     <>
+      <h1>{
+      player === 'artist' ?
+      'Your turn to draw!':
+      'Guess the drawing' 
+      }</h1>
+      <h2>{
+      player === 'artist' ?
+      'Topic: ' + correctAnswer:
+      '' 
+      }</h2>
       <Canvas
         selectedTool={ selectedTool }
         fillColorChecked={ fillColorChecked }
@@ -39,13 +92,16 @@ const Play = () => {
         setBrushWidth={ setBrushWidth }
         setRenderColor={ setRenderColor }
       />
+      { player === 'artist' ?      
       <Artist
         setSelectedTool={ setSelectedTool }
         setFillColorChecked={ setFillColorChecked }
         setBrushWidth={ setBrushWidth }
         setRenderColor={ setRenderColor }
-      />
+      /> :
       <Guess />
+      }
+      <p className="timer">{timer}</p>
 
     </>
   )
