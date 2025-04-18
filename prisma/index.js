47e6ddab6cc7d62f584ae2a0feb39a35a1ff31e1
EngineerMoore <alexiashalise@gmail.com>
2025-04-18 16:08:03 -0500

    added login and sign up db extensions

---
+++ prisma/index.js
@@ -0,0 +1,31 @@
+const bcrypt = require(`bcrypt`);
+const { PrismaClient } = require(`@prisma/client`);
+
+const prisma = new PrismaClient().$extends({
+  model: {
+    user: {
+      async signUp(username, email, firstName, lastName, password) {
+        const hash = await bcrypt.hash(password, 10);
+        const user = await prisma.user.create({
+          data: { 
+            username,
+            email,
+            firstName,
+            lastName,
+            password: hash
+          },
+        });
+        return user;
+      },
+
+      async login(email, password) {
+        const user = await prisma.user.findUniqueOrThrow({ where: { email }, });
+        const valid = await bcrypt.compare(password, user.password);
+        if (!valid) throw Error("Invalid password");
+        return user;
+      },
+    },
+  },
+});
+
+module.exports = prisma;
