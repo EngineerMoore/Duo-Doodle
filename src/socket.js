2b99b0c64685ac7420c534d5047ae83078bc7e0d
EngineerMoore <alexiashalise@gmail.com>
2025-04-27 19:29:37 -0500

    placed socket url in quotes

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import { io } from `socket.io-client`;
 
-const URL = process.env.NODE_ENV === `production` ? https://duo-doodle-socket-io.onrender.com : `http://localhost:3000`;
+const URL = process.env.NODE_ENV === `production` ? `https://duo-doodle-socket-io.onrender.com` : `http://localhost:3000`;
 
 export const socket = io(URL);
