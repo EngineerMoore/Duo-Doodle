cfea345adfde450e0a73f35e4bec6a91b187b6d0
EngineerMoore <alexiashalise@gmail.com>
2025-05-09 20:39:54 -0500

    styled results page

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -4,21 +4,23 @@ const Results = ({ correctAnswer, wrongAnswers }) => {
   const navigate = useNavigate();
 
   return (
-    <div className="holographic-background">
-      <div className="results-container">
-        <h1>Results</h1>
-        <p>
-          <strong>Correct Answer:</strong> {correctAnswer}
-        </p>
-        <h2>Wrong Attempts:</h2>
-        <ul>
+    <>
+      <h1 className="results-heading">Results</h1>
+      <div className="results">
+        <h2>Correct Answer</h2>
+        <h3 id="correct">{correctAnswer}Cat</h3>
+        <h2>Wrong Attempts</h2>
+        <ul className="wrong-results">
           {wrongAnswers.map((answer, index) => (
             <li key={index}>{answer}</li>
           ))}
+          <li>high</li>
+          <li>high</li>
+          <li>high</li>
         </ul>
         <button onClick={() => navigate("/play")}>Play Again</button>
       </div>
-    </div>
+    </>
   );
 };
 
