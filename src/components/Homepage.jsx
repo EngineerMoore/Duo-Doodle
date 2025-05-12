95f95446787dc52b65b3b9621ff41b004f4eaaa0
EngineerMoore <alexiashalise@gmail.com>
2025-05-12 04:28:55 -0500

    fixing deployment error with demo images

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router";
 import { socket } from "../socket";
-const artistView = new URL(`../assets/artist-view.png`, import.meta.url).href
-const guesserView = new URL(`../assets/guesser-view.png`, import.meta.url).href
+const artistView = new URL(`/artist-view.png`, import.meta.url).href
+const guesserView = new URL(`/guesser-view.png`, import.meta.url).href
 
 const Homepage = ({ token }) => {
   const navigate = useNavigate();
